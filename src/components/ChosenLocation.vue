b50170700a28b5d59a5273fc5ff47d81223bb473
keymaster <ihar.kliuchnik.apps@gmail.com>
2019-05-28 10:08:11 +0300

    3rd screen -  refactor with fav btn

--- src/components/ChosenLocation.vue
+++ src/components/ChosenLocation.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <h1>Property details</h1>
-    <button @click="filterAndPushToLocalStorage(location)">add to favorites</button>
+    <button v-if="typeOfBtn" @click="pushToFavorites(location)">add to favorites</button>
+    <button v-else @click="removeFromFavorites(location)">add to favorites</button>
     <div>
       <h2>{{location.price_formatted}}</h2>
       <h3>{{location.title}}</h3>
@@ -15,37 +16,37 @@
 <script>
 import * as types from "../store/types";
 import { mapGetters, mapActions } from "vuex";
-import getProperty from '../utils/getProperty';
+import getProperty from "../utils/getProperty";
 export default {
   computed: {
     ...mapGetters({
       location: types.CHOSEN_LOCATION,
       favorites: types.FAVORITES
     }),
+    typeOfBtn(location) {
+      this.favorites.some(el => el.title === getProperty(["title"], location)) ? true :false 
+    }
   },
   methods: {
     ...mapActions({
       addToFavorites: types.UPDATE_FAVORITES,
-      removeFromFavorites:types.MINUS_FROM_FAVORITES
+      deleteFromFavorites: types.MINUS_FROM_FAVORITES
     }),
-      filteredFavorites(location){
-        const updatedFavorites = this.favorites.filter(el=>{
-         return el.title !== location.title
-        });
-        return updatedFavorites;
-      },
-      filterAndPushToLocalStorage(location){
-        if(this.favorites.some(el=>el.title === getProperty(['title'],location))){
-          this.removeFromFavorites(this.filteredFavorites(location));
-          this.addToFavorites(location);
-          console.log(this.favorites);       
-        }
-        else{
-          this.addToFavorites(location);
-          console.log(this.favorites);
-        }   
-      }
-    //---------------------------------------------------------------------
+    filteredFavorites(location) {
+      const updatedFavorites = this.favorites.filter(el => {
+        return el.title !== location.title;
+      });
+      return updatedFavorites;
+    },
+    pushToFavorites(location) {
+      this.addToFavorites(location);
+      console.log(this.favorites);
+    },
+    removeFromFavorites(location) {
+      this.deleteFromFavorites(this.filteredFavorites(location));
+      this.addToFavorites(location);
+      console.log(this.favorites);
+    },
   }
 };
 </script>
